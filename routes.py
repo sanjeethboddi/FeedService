c3f3a68b4549f5dc47211e773211b03104cd1056
Sanjeeth Boddinagula <sanjeethboddi@gmail.com>
2023-01-02 15:51:38 -0600

    Minor changes

--- routes.py
+++ routes.py
@@ -17,11 +17,11 @@ DB = "feed"
 @router.get("/getFeedForUser/{user_id}/{token}")
 def getFeedForUser(token:str, request:Request, response:Response, user_id:str):
     resp =  requests.post(request.app.auth_service+f"/verify/{token}")
-    userID = str(resp.json()["username"]).lower()
-    if  resp.status_code != 200 or userID != user_id:
+    
+    if  resp.status_code != 200 or str(resp.json()["username"]).lower() != user_id:
         raise HTTPException(status_code=401, detail="Unauthorized")
-    response =  request.app.database[DB].find({"_id": userID})
-    return [i for i in response]
+    response =  request.app.database[DB].find({"_id": user_id})
+    return response[0]["posts"]
 
 @router.patch("/updateFeedDataForFollowers/{userID}/{postID}/{token}")
 def updateFeedDataForFollowers(token:str, userID:str, postID:str, request: Request, response: Response):
@@ -37,5 +37,8 @@ def updateFeedDataForFollowers(token:str, userID:str, postID:str, request: Reque
     for follower in followers:
         print("Updating feed for follower: ", str(follower))
         request.app.database[DB].update_one({"_id": follower},{"$push": { "posts": { "$each": [postID], "$slice": 500, "$position": 0 }}}, upsert=True)
+    # add post to user's feed
+    request.app.database[DB].update_one({"_id": userID},{"$push": { "posts": { "$each": [postID], "$slice": 500, "$position": 0 }}}, upsert=True)
+
     
 
